f73c12c9b806af0a300c0748a26792fc7593400a
Jayasagar Jagirapu <jayasagar@gmail.com>
2019-07-31 12:47:03 +0200

    First commit

---
+++ src/main/kotlin/com/jay/spring/kotlin/learnspringkotlin/LearnSpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.jay.spring.kotlin.learnspringkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnSpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnSpringKotlinApplication>(*args)
+}
---
+++ src/main/kotlin/com/jay/spring/kotlin/learnspringkotlin/controller/HelloController.kt
@@ -0,0 +1,12 @@
+package com.jay.spring.kotlin.learnspringkotlin.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String) = "Hello, $name"
+}
